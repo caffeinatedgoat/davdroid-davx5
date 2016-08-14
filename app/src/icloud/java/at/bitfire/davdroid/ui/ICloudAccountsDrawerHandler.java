c14653c08add70afd77b6191884b611c67f364fd
Ricki Hirner <hirner@bitfire.at>
2016-08-14 14:08:28 +0200

    Navigation drawer, read-only settings

--- app/src/icloud/java/at/bitfire/davdroid/ui/ICloudAccountsDrawerHandler.java
+++ app/src/icloud/java/at/bitfire/davdroid/ui/ICloudAccountsDrawerHandler.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 
+import at.bitfire.davdroid.Constants;
 import at.bitfire.davdroid.R;
 
 public class ICloudAccountsDrawerHandler implements IAccountsDrawerHandler {
@@ -14,6 +15,12 @@ public class ICloudAccountsDrawerHandler implements IAccountsDrawerHandler {
         switch (item.getItemId()) {
             case R.id.nav_app_license:
                 activity.startActivity(new Intent(activity, SubscriptionActivity.class));
+                break;
+            case R.id.nav_app_settings:
+                activity.startActivity(new Intent(activity, AppSettingsActivity.class));
+                break;
+            case R.id.nav_website:
+                activity.startActivity(new Intent(Intent.ACTION_VIEW, Constants.webUri));
                 break;
             default:
                 return false;
